7c6b8ce7a665ff9217183cc03ea46dda1773ad71
Fathima-Ismail <138851981+Fathima-Ismail@users.noreply.github.com>
2023-11-24 23:14:05 +0530

    Create welcome_and_slice.js

---
+++ welcome_and_slice.js
@@ -0,0 +1,6 @@
+// To print in console
+console.log("Hi Hello World! Welcome");
+console.log('Hello World');
+
+// Slicing the argument - while entering file name, enter the sentence to be sliced as well
+console.log(process.argv.slice(2));
